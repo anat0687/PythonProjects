55185de96cae5867815b1722eead300e198ea843
Andrew Atkinson <64949404+anat0687@users.noreply.github.com>
2020-06-02 21:53:49 -0700

    working table code

--- working_with_strings.py
+++ working_with_strings.py
@@ -1,14 +1,27 @@
-def printTable(data):
-    count = 0
+def printtable(data):
+    width = 0
+    tjust = []
+    row = 0
     for i in data:
         for j in i:
-            if len(j) > count:
-                count = len(j)
-            print(j)
-    print(count)
+            if len(j) > width:
+                width = len(j)
+    for i in data:
+        col = 0
+        rowjust = []
+        for j in i:
+            jjj = j.rjust((width+3))
+            # print(jjj)
+            rowjust.append(jjj)
+            col += 1
+        tjust.append(rowjust)
+        row += 1
+    for i in tjust:
+        print(i)
 
 tableData = [['apples', 'oranges', 'cherries', 'banana'],
              ['Alice', 'Bob', 'Carol', 'David'],
              ['dogs', 'cats', 'moose', 'goose']]
 
-printTable(tableData)
+printtable(tableData)
+
