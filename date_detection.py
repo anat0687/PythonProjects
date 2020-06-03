d7c4c0904286471d763a1a09247c3faacb0dc0d4
Andrew Atkinson <64949404+anat0687@users.noreply.github.com>
2020-06-03 01:25:20 -0700

    date function formats try1

--- date_detection.py
+++ date_detection.py
@@ -1,15 +1,25 @@
 import re
 
-def isdate(string):
 
-    if len(string) != 10:
-        return False
-    if (string[2] or string[5]) != '/':
-        return False
-    else:
-        return True
+def dateword(datestr):
+    months = ["january", "feb", "mar", "apr", "may", "june",
+              "july", "aug", "sep", "oct", "nov", "dec"]
+    phoneNumRegex = re.compile(r'(\d\d)/(\d\d)/(\d\d\d\d)')
+    mo = phoneNumRegex.search(test_string)
+    #print(mo.groups())
+    day = int(mo.group(1))
+    month = int(mo.group(2))
+    year = int(mo.group(3))
+    if day > 31:
+        print("not a valid date")
+        return
+    if month > 12:
+        print("not a valid date")
+        return
+    monthword = months[month-1]
+    print(monthword + mo.group(1) + mo.group(2))
 
 
 test_string = input("please enter a date:\n")
 
-print(isdate(test_string))
+dateword(test_string)
