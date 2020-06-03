803977dd33d15b9c945ab76f7e56606b3373f9fd
Andrew Atkinson <64949404+anat0687@users.noreply.github.com>
2020-06-03 00:43:41 -0700

    date function no comment

--- date_detection.py
+++ date_detection.py
@@ -1,3 +1,15 @@
-string = input("please enter a date:\n")
+import re
 
-print(f'you entered {string}')
+def isdate(string):
+
+    if len(string) != 10:
+        return False
+    if (string[2] or string[5]) != '/':
+        return False
+    else:
+        return True
+
+
+test_string = input("please enter a date:\n")
+
+print(isdate(test_string))
