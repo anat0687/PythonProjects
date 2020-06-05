0e4da81a5b11a96084d2c18d8a9afb50ac7858fd
andrewat <andrew@theatkinsons.org>
2020-06-05 16:09:09 -0700

    euler problem 3 solved

--- euler_problem_3.py
+++ euler_problem_3.py
@@ -1,22 +1,19 @@
 
-from math import floor
 
-
-test_num = 51475143
+test_num = 600851475143
 mods = test_num
-
 factors = []
-
 i = 2
 
-while i < floor(mods):
+
+while i <= mods:
     if test_num % i == 0:
-        mods = test_num
         test_num = test_num / i
+        mods = test_num
         factors.append(i)
         i = 1
     i += 1
 
 
-print(factors)
+print(max(factors))
 
