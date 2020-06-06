08ad65900cb5e22f8cde5c3f6740e9b6f4f6c311
Andrew Atkinson <64949404+anat0687@users.noreply.github.com>
2020-06-06 16:56:38 -0700

    euler problem 12 solved, very slow though

---
+++ euler_problem_12.py
@@ -0,0 +1,29 @@
+
+
+def triangle_num(num):
+    tnum = sum(range(num+1))
+    return tnum
+
+
+def getdivs(n):
+    divs = []
+    for j in range(1, n+1):
+        if n % j == 0:
+            divs.append(j)
+
+    return divs
+
+
+
+# for i in range(1, 8):
+# #    print(triangle_num (i))
+#     print(getdivs(triangle_num(i)))
+
+i = 1
+
+while len(getdivs(triangle_num(i))) < 500:
+    i += 1
+
+print(triangle_num(i))
+print(getdivs(triangle_num(i)))
+
